2a85499da803797cb615739d395642d335902a7b
unknown <ajn252@gmail.com>
2021-04-07 22:54:50 -0400

    initial commit

---
+++ db/migrate/20210408022158_create_users.rb
@@ -0,0 +1,14 @@
+class CreateUsers < ActiveRecord::Migration[6.1]
+  def change
+    create_table :users do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :suffix
+      t.string :email
+      t.string :password
+      t.string :password_confirmation
+
+      t.timestamps
+    end
+  end
+end
